96c99582f45d7f87861680068478c994e117f328
FaisalMS <fis445@hotmail.com>
2024-01-01 16:15:48 +0300

    V1.1

--- src/App.java
+++ src/App.java
@@ -2,11 +2,16 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int theWeight = 55;
+        
 
         System.out.println("Hello Welcome To The BMI Calculator ");
-        
+
         Scanner s = new Scanner(System.in);
+        double theHeight = s.nextDouble();
+        int theWeight = s.nextInt();
+        double theResult = theWeight / theWeight;
+        
+        System.out.println("Please Enter Your Height : " + theHeight);
 
     }
 }
