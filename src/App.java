e9316ca99de246a307edf353cf7094ba35326c94
FaisalMS <fis445@hotmail.com>
2024-01-01 16:03:49 +0300

    V1

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int theWeight = 55;
+
+        System.out.println("Hello Welcome To The BMI Calculator ");
+        
+        Scanner s = new Scanner(System.in);
+
+    }
+}
